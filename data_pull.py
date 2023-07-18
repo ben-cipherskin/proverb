3187382b8a7c5a5a973e8d5c0f6c0e70fde3a906
ben-cipherskin <72762527+ben-cipherskin@users.noreply.github.com>
2023-07-17 22:35:51 -0400

    data pull added and main sit to stand updated

---
+++ data_pull.py
@@ -0,0 +1,34 @@
+"""
+Script used to pull the data from AWS and to be put in in this repository /data/... Both data from the sleeves and
+peripheral devices will be pulled.  This is data is needed for the main programs to work. It pulls the data directly
+from the AWS, no local syncing.
+
+It also syncs the chronojumps database to AWS and saves it
+
+Last updated: 2022-04-01
+by: Simon Grannetia
+"""
+from pathlib2 import Path
+import boto3
+
+from data_processing_folder.file_system_ops import make_folder
+from data_processing_folder.parsing_zip import data_pull
+
+s3 = boto3.resource('s3')
+
+# Variables
+check_if_exists = True
+pull_experiment = ['sit_to_stand']
+all_data_to = Path.cwd() / 'data' / 'raw'
+make_folder(all_data_to)
+max_exp_to_grab = 20
+periph_bucket_name = 'data-science-peripheral-devices'
+periph_data = s3.Bucket(periph_bucket_name)  # Amazon s3 bucket with all data for peripheral devices
+sleeve_data = s3.Bucket('digital-mirror')  # S3 bucket with sleeve data
+all_data_to = Path.cwd() / 'data' / 'raw'   # where all the files are stored in file structure locally
+reference_file = Path.cwd() / 'references' / 'files_to_grab.json'
+data_pull(pull_experiment, max_exp_to_grab, check_if_exists, all_data_to=all_data_to, reference_file=reference_file,
+          experiment_types=pull_experiment, devices=['sleeve'])
+
+# Pushing peripheral data if not on aws
+# upload_to_aws(periph_data, all_data_to, [''])
--- main_sit_to_stand.py
+++ main_sit_to_stand.py
@@ -1,23 +1,133 @@
 import gzip
 import json
+import glob
+import os
+
+from data_processing_folder.file_system_ops import get_all_jsons
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from pathlib2 import Path
+
+# Specify the directory path and pattern
+from dsp import time_to_seconds
+from grab_data import get_gyro, get_gyro_rms, orient_acc
+
+directory = '/Users/cipherskin/PycharmProjects/proverb/data/manually_added/'
+pattern = '*Sit_To_Stand*'
+
+person_check = ["Suzanne1"]
+date_check = ["20230710"]
+experiment_check = ["sit_to_stand"]
+save_fig = True
 
 
-file = '/Users/cipherskin/PycharmProjects/proverb/data/manually_added/'
+data_from = Path.cwd() / "data" / "raw" / "sit_to_stand"
+reference_json = Path.cwd() / "references" / "devices_experiment.json"
+all_exps, all_devices, exps_info = get_all_jsons(data_from, reference_json, person_check=person_check,
+                                                 date_check=date_check, experiment_check=experiment_check, devices=[''])
 
-if '6_Minute_Walk_Test' in str(file):
-    pass
-else:
-    print('Not A 6MWT')
+# Get all files matching the pattern in the directory
+files = glob.glob(os.path.join(directory, pattern))
 
-try:
-    # Fetch the file from locally stored directory
+# Iterate through each file
+for file in files:
+    print("Processing file:", file)
     with gzip.open(file) as fin:
         data = fin.read()
         data = json.loads(data.decode('utf-8'))
 
+    data['rawData']['sit_to_stand_count'] = 0
+    gyros = get_gyro(data, sleeve_num=0)
+    prox_gyro_t, prox_gyro, dist_gyro_t, dist_gyro = gyros[0], gyros[1], gyros[2], gyros[3]
+    gyro_rms = get_gyro_rms(data, filt=True)
+    acc_prox_times, prox_x, prox_y, prox_z, acc_dist_times, dist_x, dist_y, dist_z = orient_acc(data)
+    acc_prox_times = time_to_seconds(acc_prox_times)
+    acc_dist_times = time_to_seconds(acc_dist_times)
+
+    fig = make_subplots(
+        rows=4,
+        cols=1,
+        subplot_titles=("Proximal Acc", "Distal Acc", "Proximal Gyro", "Distal Gyro"),
+        shared_xaxes=True
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=acc_prox_times,
+            y=prox_x,
+            mode='lines',
+            name=f"Prox X",
+            visible=True,
+            legendgroup=1
+        ),
+        row=1,
+        col=1
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=acc_prox_times,
+            y=prox_y,
+            mode='lines',
+            name=f"Prox Y",
+            visible=True,
+            legendgroup=1
+        ),
+        row=1,
+        col=1
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=acc_prox_times,
+            y=prox_z,
+            mode='lines',
+            name=f"Prox Z",
+            visible=True,
+            legendgroup=1
+        ),
+        row=1,
+        col=1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=acc_dist_times,
+            y=dist_x,
+            mode='lines',
+            name=f"Dist X",
+            visible=True,
+            legendgroup=2
+        ),
+        row=2,
+        col=1
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=acc_dist_times,
+            y=dist_y,
+            mode='lines',
+            name=f"Dist Y",
+            visible=True,
+            legendgroup=2
+        ),
+        row=2,
+        col=1
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=acc_dist_times,
+            y=dist_z,
+            mode='lines',
+            name=f"Dist Z",
+            visible=True,
+            legendgroup=2
+        ),
+        row=2,
+        col=1
+    )
+
+    fig.show()
+
+    final_count = 0
     if 'sit_to_stand_count' in data['rawData'].keys():
         final_distance = data['rawData']['sit_to_stand_count']
-        print(f'Distance: {final_distance} m')
+        print(f'Final Count: {final_count} m')
 
-except:
-    print('Error')
